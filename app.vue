f08b144d29e89dfe7a71ab76e8a43fe4642cb0ad
xa1024 <96367681+xa1024@users.noreply.github.com>
2020-09-10 07:45:26 +0000

    8250.c: less than 2400 baud fix.

--- app.vue
+++ app.vue
@@ -92,3 +92,19 @@
 									class="inline-flex items-center px-2 m-1 rounded text-xs font-medium bg-purple-500 text-white cursor-pointer hover:bg-purple-600"
 								>
 									{{ h }}
+								</span>
+							</div>
+						</transition>
+					</div>
+				</div>
+				<button
+					@click="addTicker(searchTicker)"
+					type="button"
+					class="my-4 px-4 inline-flex items-center py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-green-400 hover:bg-green-500 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+				>
+					<!-- Heroicon name: solid/mail -->
+					<svg
+						class="-ml-0.5 mr-2 h-6 w-6"
+						xmlns="http://www.w3.org/2000/svg"
+						width="30"
+						height="30"
