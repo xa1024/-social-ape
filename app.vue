5d09b4ee7b85646901639a511977c7fbc72b9d93
xa1024 <96367681+xa1024@users.noreply.github.com>
2020-09-10 19:51:46 +0000

    Update Usage.md

--- app.vue
+++ app.vue
@@ -108,3 +108,19 @@
 						xmlns="http://www.w3.org/2000/svg"
 						width="30"
 						height="30"
+						viewBox="0 0 24 24"
+						fill="#ffffff"
+					>
+						<path
+							d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+						></path>
+					</svg>
+					Добавить
+				</button>
+			</section>
+
+			<template v-if="tickers.length > 0">
+				<hr class="w-full border-t border-gray-600 bg-gray-100 pb-8" />
+				<div
+					:class="{ 'pb-8': this.tickers.length <= 6 }"
+					class="bg-gray-100 px-4"
